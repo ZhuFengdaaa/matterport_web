a5ba8e467c7693c019b9c93108625fd873a664f4
liangcici <liangcici5@gmail.com>
2020-02-10 17:24:57 +0800

    test

--- app/js/Matterport3D.js
+++ app/js/Matterport3D.js
@@ -70,8 +70,8 @@ Matterport3D.prototype.load_viewpoints = function(data, {h=0.5, opacity=0.7} = {
       }
     }
 
-    var box_geometry = new THREE.BoxGeometry(0.15, 0.15, 1)
-    var box_material = new THREE.MeshLambertMaterial({color: 0x0000ff});
+    var box_geometry = new THREE.BoxGeometry(1, 1, 1)
+    var box_material = new THREE.MeshLambertMaterial({color: 0x00ff00});
     // box_material.transparent = true;
     box_material.opacity = opacity;
     var boxes = new THREE.Mesh(box_geometry, box_material);
